5dfadfd32f6ab6c5eeac41741c42436b9fdc700e
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2020-09-19 23:49:53 +0530

    Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./assets/scss/App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Initital</header>
+      <header className="App-header">Initital Code</header>
     </div>
   );
 }
