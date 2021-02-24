68f6928ac5e52b1d56242d395ea84775dcd446d7
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-02-25 00:09:03 +0530

    Passed projects through props

--- client/src/components/Kanban.jsx
+++ client/src/components/Kanban.jsx
@@ -1,42 +1,45 @@
+import { useState, useEffect } from "react";
 import "../assets/scss/kanban.scss";
 
-const Kanban = () => {
+const Kanban = ({ project }) => {
+	// return Kanban board if project is found
 	return (
-		<>
-			<div className="kanban">
-				<div className="col">
-					<h3>To Do</h3>
-					<div className="card high">
-						<h4>Title</h4>
-						<date>23-08-2021</date>
-						Description
-					</div>
-					<div className="card mid">
-						<h4>Title</h4>
-						<date>23-08-2021</date>
-						Description
-					</div>
-				</div>
-				<div className="col">
-					<h3>In Progress</h3>
+		<div className="kanban">
+			<div className="col">
+				<h3>To Do</h3>
 
-					<div className="card">
-						<h4>Title</h4>
-						<date>23-08-2021</date>
-						Description
-					</div>
+				<div className="card high">
+					<h4>Title</h4>
+					<span className="date">23-08-2021</span>
+					Description
 				</div>
-				<div className="col">
-					<h3>Done</h3>
-
-					<div className="card">
-						<h4>Title</h4>
-						<date>23-08-2021</date>
-						Description
-					</div>
+				<div className="card mid">
+					<h4>Title</h4>
+					<span className="date">23-08-2021</span>
+					Description
 				</div>
 			</div>
-		</>
+
+			<div className="col">
+				<h3>In Progress</h3>
+
+				<div className="card">
+					<h4>Title</h4>
+					<span className="date">23-08-2021</span>
+					Description
+				</div>
+			</div>
+
+			<div className="col">
+				<h3>Done</h3>
+
+				<div className="card">
+					<h4>Title</h4>
+					<span className="date">23-08-2021</span>
+					Description
+				</div>
+			</div>
+		</div>
 	);
 };
 
