8d2d52fb4310ce07b3a2fc9fe570f2b4ab874896
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-03-13 01:38:30 +0530

    Navbar Fixes?

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
 	const { authUser, notifications, db, userData, logout, project } = useContext(
 		DataStore
 	);
+
 	const accept = (projectId) => {
 		let index = -1;
 		db.doc("projects" + projectId)
@@ -44,34 +45,23 @@ const Navbar = () => {
 				}
 			});
 	};
+
 	return (
 		<nav>
 			<div className="navbar">
 				<div className="start">
 					{/* Display Logo */}
-					<span className="nav-item">
-						<NavLink className="brand" to="/">
+					<span className="nav-item brand">
+						<NavLink className="" to="/">
 							<img src={Logo} alt="" />
 							{/* Brand Name if on Home */}
-							<Route exact path="/" render={() => "LightHouse"} />
+							<Route
+								exact
+								path={["/", "/kanban", "/mindmap", "/login"]}
+								render={() => "LightHouse"}
+							/>
 						</NavLink>
 					</span>
-
-					{/* On some project page */}
-					<Route
-						path="/p/:projectID"
-						component={() => (
-							<span className="nav-item">
-								<NavSignedProject name={project?.name} />
-							</span>
-						)}
-					/>
-					<Route
-						exact
-						path="/projects"
-						render={() => <span className="nav-item"> Your Projects </span>}
-					/>
-
 					{/* Show Kanban/Mindmap if not logged in */}
 					{!authUser[0] ? (
 						<Route path="/">
@@ -83,25 +73,47 @@ const Navbar = () => {
 							</span>
 						</Route>
 					) : null}
+
+					{/* On some project page */}
+					{authUser[0] ? (
+						<>
+							<Route
+								path="/p/:projectID"
+								component={() => <NavSignedProject name={project?.name} />}
+							/>
+							<Route
+								exact
+								path="/projects"
+								render={() => (
+									<span className="nav-item">
+										<NavLink to="/projects">Your Projects</NavLink>
+									</span>
+								)}
+							/>
+						</>
+					) : null}
 				</div>
 				<div className="end">
 					{/* Notification and Profile Icons if logged in else Login */}
 					{authUser[0] ? (
 						<>
-							<div className="nav-item dropdown">
-								<div className="image">
-									<img src={NotifBell} alt="" />
-								</div>
+							<span
+								className={`nav-item bell ${
+									notifications ? "unseen" : null
+								} dropdown`}>
+								<img src={NotifBell} alt="" />
 								<div className="dropdown-box"></div>
-							</div>
-							<div className="nav-item profile dropdown">
+							</span>
+							<span className="nav-item profile dropdown">
 								<img src={userData?.photoURL} alt="" />
 								<div className="dropdown-box">
-									<a onClick={logout}>Sign Out</a>
+									<a href="/" onClick={logout}>
+										Sign Out
+									</a>
 									<a>Your Profile</a>
 									<a>Settings</a>
 								</div>
-							</div>
+							</span>
 						</>
 					) : (
 						<span className="nav-item">
@@ -116,16 +128,17 @@ const Navbar = () => {
 
 const NavSignedProject = ({ name }) => {
 	const { projectID } = useParams();
+
 	return (
 		<>
 			{/* Name of Project and Kanban/Mindmap Links */}
 			<span>{name}</span>
-			<NavLink to={`/p/${projectID}/kanban`} className="nav-item">
-				Kanban
-			</NavLink>
-			<NavLink to={`p/${projectID}/mindmap`} className="nav-item">
-				Mind Map
-			</NavLink>
+			<span className="nav-item">
+				<NavLink to={`/p/${projectID}/kanban`}>Kanban</NavLink>
+			</span>
+			<span className="nav-item">
+				<NavLink to={`/p/${projectID}/mindmap`}>Mind Map</NavLink>
+			</span>
 		</>
 	);
 };
