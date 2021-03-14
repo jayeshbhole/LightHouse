494176ba40f0edbaa91dfee12ebe93f0f7bab36c
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-03-14 15:35:30 +0530

    Temporary rendering without DND

--- client/src/components/Kanban.jsx
+++ client/src/components/Kanban.jsx
@@ -1,41 +1,70 @@
 import { DragDropContext } from "react-beautiful-dnd";
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "../assets/scss/kanban.scss";
 
 const Kanban = ({ project }) => {
+	const [cols, setCols] = useState({ todo: [], inpro: [], done: [] });
+
+	useEffect(() => {
+		const cards = Object.values(project?.cards || {});
+		if (cards) {
+			const columns = { todo: [], inpro: [], done: [] };
+			cards.forEach((card) => columns[card.status].push(card));
+			setCols(columns);
+		}
+	}, [project?.cards]);
+
+	useEffect(() => {
+		console.log(cols);
+	}, [cols]);
+
+	const toDate = (deadline) => {
+		const date = deadline?.toDate().toString().split(" ");
+		return `${date[2]} ${date[1]} ${date[3]}`;
+	};
 	return (
 		<DragDropContext>
-			<div className="kanban">
+			<div className="kanban page">
 				<div className="col">
 					<h3>To Do</h3>
-
-					<div className="card high">
-						<h4>Title</h4>
-						<span className="date">23-08-2021</span>
-						Description
-					</div>
-					<div className="card mid">
-						<h4>Title</h4>
-						<span className="date">23-08-2021</span>
-						Description
+					<div className="cards">
+						{cols.todo.map((card, id) => {
+							return (
+								<div key={id} className={`card ${card.priority}`}>
+									<h4>{card.title}</h4>
+									<div className="desc">{card.desc}</div>
+									<span className="date">{toDate(card.deadline)}</span>
+								</div>
+							);
+						})}
 					</div>
 				</div>
 				<div className="col">
 					<h3>In Progress</h3>
-
-					<div className="card">
-						<h4>Title</h4>
-						<span className="date">23-08-2021</span>
-						Description
+					<div className="cards">
+						{cols.inpro.map((card, id) => {
+							return (
+								<div key={id} className={`card ${card.priority}`}>
+									<h4>{card.title}</h4>
+									<div className="desc">{card.desc}</div>
+									<span className="date">{toDate(card.deadline)}</span>
+								</div>
+							);
+						})}
 					</div>
 				</div>
 				<div className="col">
 					<h3>Done</h3>
-
-					<div className="card">
-						<h4>Title</h4>
-						<span className="date">23-08-2021</span>
-						Description
+					<div className="cards">
+						{cols.done.map((card, id) => {
+							return (
+								<div key={id} className={`card ${card.priority}`}>
+									<h4>{card.title}</h4>
+									<div className="desc">{card.desc}</div>
+									<span className="date">{toDate(card.deadline)}</span>
+								</div>
+							);
+						})}
 					</div>
 				</div>
 			</div>
